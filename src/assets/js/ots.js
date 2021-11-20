9decd68424e96ec6408208925338dbf49f24568d
Rahul Kumar <52167870+wukong23-cloud@users.noreply.github.com>
2021-11-20 17:35:43 +0530

    Delete src directory

--- src/assets/js/ots.js
+++
@@ -1,9 +0,0 @@
-// var form = document.getElementById("test");
-// var selectedOption = $("input:radio[name=age]:checked").val()
-// alert(selectedOption);
-
-document.test.onclick = function(){
-  var radVal = document.test.age.value;
-  result = 'You selected: '+radVal;
-  alert(result)
-}
--- src/subject.ts
+++
@@ -1,6 +0,0 @@
-export interface Subject{
-    qid: number;
-    subject_id:number;
-    subject_name:string;
-
-}
